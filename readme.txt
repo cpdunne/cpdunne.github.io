96c1e107d8c731c322ebccbd991f3e51b6919e40
Colin <83467754+cpdunne@users.noreply.github.com>
2022-06-14 16:51:28 -0400

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,8 @@
+NTLab4: turtles
+squares.py
+
+Directions:
+When prompted, input the number of squares you want to be drawn and press 'Enter'. When done, click anywhere on the yellow turtle graphics screen to close it.
+
+Challenge:
+The biggest challenge I encountered was I approached thinking about the algorithm wrong, where I imagined the number of squares adding as it would draw instead of subtracting. This meant I approached it thinking I could set a parameter detecting if the number of squares left were positive, which wouldn't work in many instances.
---
+++ squares.py
@@ -0,0 +1,54 @@
+import turtle               # import the turtle module 
+import math                 # import the math module
+
+numsquares = int(input("Number of squares to draw: "))
+numsquares -= 1
+
+screen = turtle.Screen()    # open a turtle screen 
+screen.bgcolor("yellow")    # set screen's background color
+
+ted = turtle.Turtle()       # create a turtle; call it ted
+
+sideLength = 300            #set length of first square's side to 300
+
+for i in range(4):              # draw a square: do the following 4 times
+     ted.forward(sideLength)    # draw a line 150 pixels long
+     ted.left(90)               # turn left to be ready for next line
+
+if numsquares > 0:
+     ted.goto(sideLength / 2, 0)
+     ted.left(45)
+     sideLength = sideLength/ math.sqrt(2)
+     ted.forward(sideLength)
+     for i in range(3):              # draw a square: do the following 4 times
+          ted.left(90)
+          ted.forward(sideLength)    # draw a line 150 pixels long
+     numsquares -= 1
+
+while numsquares > 0:
+     ted.penup()
+     sideLength = sideLength/ math.sqrt(2)
+     ted.goto(150 - sideLength / 2, 150 - sideLength / 2)
+     ted.pendown()
+     ted.left(45)
+     ted.forward(sideLength)
+     for i in range(3):              # draw a square: do the following 4 times
+          ted.left(90)
+          ted.forward(sideLength)
+     numsquares -= 1
+     if numsquares > 0:
+          ted.penup()
+          ted.goto(150 - sideLength/ 2, 150)
+          sideLength = sideLength/ math.sqrt(2)
+          ted.pendown()
+          ted.left(45)
+          ted.forward(sideLength)
+          for i in range(3):              # draw a square: do the following 4 times
+               ted.left(90)
+               ted.forward(sideLength)
+          ted.left(90)
+          numsquares -= 1
+
+     
+
+screen.exitonclick()        # close screen when user clicks on it
